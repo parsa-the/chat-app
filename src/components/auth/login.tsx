abcad90daa73ec3ca5fa2837be7f40dce9c23a43
parsa_the <parsaparvizi018@gmail.com>
2025-09-12 21:31:29 +0900

    login design

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,5 +1,8 @@
-"use client"
-{/*add reset password functionality */}
+"use client";
+{
+  /*add reset password functionality */
+}
+import Link from "next/link";
 import React, { useState } from "react";
 type LoginTypes = {
   email: string;
@@ -24,25 +27,34 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handlesubmit}>
-      <input
-        type="email"
-        name="email"
-        value={LoginData.email}
-        placeholder="Enter Email"
-        onChange={handleChange}
-        required
-      ></input>
-      <input
-        name="password"
-        type="password"
-        value={LoginData.password}
-        placeholder="Enter password"
-        onChange={handleChange}
-        required
-      ></input>
-      <button>Login</button>
-    </form>
+    <div className="w-screen">
+      <form
+        onSubmit={handlesubmit}
+        className="flex flex-col items-center justify-center p-10 w-100 mx-auto mt-40 rounded-lg space-y-10 shadow-xl border border-gray-200"
+      >
+        <input
+          type="email"
+          name="email"
+          value={LoginData.email}
+          placeholder="Enter Email"
+          onChange={handleChange}
+          className="p-2 w-70 border rounded-sm font-normal"
+          required
+        ></input>
+        <input
+          name="password"
+          type="password"
+          value={LoginData.password}
+          placeholder="Enter password"
+          onChange={handleChange}
+          className="p-2 font-normal w-70 border rounded-sm"
+          required
+        ></input>
+        <button className="bg-black text-white p-2 font-medium w-70 transition duration-300 ease-in-out rounded-sm active:bg-gray-700 hover:bg-gray-800">Login</button>
+        <p >dont have an account? <Link href={"/signup"} className="text-blue-600">Create an account</Link></p>
+      </form>
+
+    </div>
   );
 };
 
--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-
+import Link from "next/link";
 const Signup = () => {
   type FormState = {
     email: string;
@@ -27,23 +27,29 @@ const Signup = () => {
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     // llllllllllllllllllllllll
-    {/* supabase later */}
-    if(FormData.password!==FormData.confirmPassword){
-      alert("repeated password doesn't match")//toast laterrrrrrrrrrrrrrrr
-      return
+    {
+      /* supabase later */
+    }
+    if (FormData.password !== FormData.confirmPassword) {
+      alert("repeated password doesn't match"); //toast laterrrrrrrrrrrrrrrr
+      return;
     }
     console.log(FormData);
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center p-10 w-100 mx-auto mt-20 rounded-lg space-y-10 shadow-xl border border-gray-200"
+      >
         <input
           type="email"
           name={"email"}
           value={FormData.email}
           onChange={handleChange}
           placeholder="Enter Email"
+          className="p-2 w-70 border rounded-sm font-normal"
           required
         ></input>
         <input
@@ -52,6 +58,7 @@ const Signup = () => {
           value={FormData.password}
           onChange={handleChange}
           placeholder="Enter password"
+          className="p-2 w-70 border rounded-sm font-normal"
           required
         ></input>
         <input
@@ -60,6 +67,7 @@ const Signup = () => {
           value={FormData.confirmPassword}
           onChange={handleChange}
           placeholder="Repeat password"
+          className="p-2 w-70 border rounded-sm font-normal"
           required
         ></input>
         <input
@@ -68,11 +76,14 @@ const Signup = () => {
           value={FormData.displayName}
           onChange={handleChange}
           placeholder="Enter your name"
+          className="p-2 w-70 border rounded-sm font-normal"
           required
         ></input>
-        <button type="submit">Sign up</button>
+        <button type="submit"  className="bg-black text-white p-2 font-medium w-70 transition duration-300 ease-in-out rounded-sm active:bg-gray-700 hover:bg-gray-800">Sign up</button>
+        <p>
+          alrady have an account? <Link href="/login" className="text-blue-600">Log in</Link>
+        </p>
       </form>
-      <p>{JSON.stringify(FormData)}</p>
     </>
   );
 };
